5dd3aae321d863cba1a8afb7d534f40ce6055afe
LeeEleven11 <121217605+LeeEleven11@users.noreply.github.com>
2025-05-22 15:38:48 +0800

    Update config.py
    HOTEL_DB_HOST

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.DEBUG)
 # 配置信息
 class Config:
     DEBUG = True
-    AWS_REGION = os.environ.get('HOTEL_DB_HOST')
+    HOTEL_DB_HOST = os.environ.get('HOTEL_DB_HOST')
     APPRUNNER_SERVICE_ARN = os.environ.get('APPRUNNER_SERVICE_ARN')
     DB_SECRET_ARN = os.environ.get('DB_SECRET_ARN')
     HOTEL_NAME = os.environ.get('HOTEL_NAME', 'Cloud Raiser Hotel')
